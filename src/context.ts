6b9cfe1facf5764db258f8f66458e52fa7bce9a6
TingzhouJia <joe.tzjia@gmail.com>
2021-04-14 23:55:18 -0400

    context

---
+++ src/context.ts
@@ -0,0 +1,16 @@
+import { Instance } from "mobx-state-tree";
+import { createContext, useContext } from "react";
+import { FormStore } from "./Form";
+import { warning } from "./utils";
+
+export type RootInstance = Instance<typeof FormStore>;
+const RootStoreContext = createContext<null | RootInstance>(null);
+export function useMst() {
+    const store = useContext(RootStoreContext);
+    if (store === null) {
+        warning(false, "No `Form` component found")
+        throw new Error("Store cannot be null, please add a context provider");
+    }
+    return store;
+}
+export const Provider = RootStoreContext.Provider;
