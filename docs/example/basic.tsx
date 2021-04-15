3c3e127fb01553ca7426897e65818d2d35b2141d
TingzhouJia <joe.tzjia@gmail.com>
2021-04-15 15:36:20 -0400

    docs

--- docs/example/basic.tsx
+++ docs/example/basic.tsx
@@ -1,11 +1,23 @@
+import { toJS } from 'mobx'
+import React, { useState } from 'react'
 import { Field, Form } from 'rong-form'
 export default () => {
-    
+    const [state, setstate] = useState<any>()
     return (
-        <Form>
-            <Field name="username">
-                <input type="text" />
-            </Field>
-        </Form>
+        <>
+            {JSON.stringify(state)}
+
+            <Form onValuesChange={(val,second) => setstate({val,second})}>
+                <Field name="username" >
+                    <input type="text" />
+                </Field>
+                <Field name="password">
+                    <input type="text" />
+                </Field>
+                <button type="submit">submit</button>
+
+            </Form>
+
+        </>
     )
 }
