2e8dfa70118174782d9280de2dd355bddb774404
TingzhouJia <joe.tzjia@gmail.com>
2021-04-16 15:29:01 -0400

    context

--- src/context.ts
+++ src/context.ts
@@ -1,7 +1,7 @@
 import { Instance } from "mobx-state-tree";
 import { createContext, useContext } from "react";
 import { FormStore } from "./formType";
-import { ValidateTriggerType } from "./interface";
+import { ValidateMessages, ValidateTriggerType } from "./interface";
 import { warning } from "./utils";
 
 export type RootInstance = Instance<typeof FormStore>;
@@ -9,7 +9,7 @@ export type RootInstance = Instance<typeof FormStore>;
 export type FormType = {
     store:RootInstance,
     validateTrigger?:ValidateTriggerType[]
-
+    validateMessage?:ValidateMessages
 }
 const RootStoreContext = createContext<null | FormType>(null);
 export function useMst() {
@@ -21,3 +21,12 @@ export function useMst() {
     return store;
 }
 export const Provider = RootStoreContext.Provider;
+
+
+export type ListFormType={
+    store:any
+    addToStore:(val:any)=>any,
+
+}
+
+export const ListStoreContext=createContext<ListFormType>(null)
