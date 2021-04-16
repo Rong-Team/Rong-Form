6cdfdc19c164f5c5a7abc2b3cd5ee58a41096425
TingzhouJia <joe.tzjia@gmail.com>
2021-04-15 21:11:56 -0400

    formType

--- src/formType.ts
+++ src/formType.ts
@@ -1,5 +1,8 @@
-import { types } from "mobx-state-tree"
+import { getEnv, types } from "mobx-state-tree"
+import { defaultValidateMessages } from "./defaultValidateMessage"
 import { FieldStore } from "./Field"
+import { Rule, ValidateMessages } from "./interface"
+import { validateRule } from "./validateUtils"
 
 export const FormStore = types.model("Form", {
     fields: types.optional(types.map(FieldStore), {})
@@ -15,30 +18,46 @@ export const FormStore = types.model("Form", {
         const cur = self.fields.get(name)
         self.fields.set(name, { ...cur, value })
     },
-    changeField(name:string,other:any){
-        const cur=self.fields.get(name)
-        self.fields.set(name,{...cur,...other})
+    changeField(name: string, other: any) {
+        const cur = self.fields.get(name)
+        self.fields.set(name, { ...cur, ...other })
     },
     reset() {
         self.fields.forEach(item => item.reset())
+    },
+    validateFields(field: string, rules: Rule) {
+        const cur = self.fields.get(field)
+        if (cur) {
+            self.fields.set(field, { ...cur, validating: true })
+            const newVali = getEnv(self).validateMessages
+            if (!rules) {
+                return
+            }
+            const promise = validateRule(field, cur.value, rules, { validateMessages: { ...defaultValidateMessages, ...newVali }, })
+            promise.then(() => {
+                self.fields.set(field, { ...cur, validating: false, error: null })
+            }).catch(e => {
+                self.fields.set(field, { ...cur, validating: false, error: e })
+            })
+        }
     }
 
 })).views((self) => ({
     getFieldValue(name: string) {
         return self.fields.get(name)?.value
     },
-    getFieldByName(name:string){
+    getFieldByName(name: string) {
         return self.fields.get(name)
     },
-    getFieldKeys(key,value){
-        let cur={}
-         self.fields.forEach((item)=>{
-            if(item.name===key){
-                cur[item.name]={name:item.name,value:value}
-            }else{
-                cur[item.name]={name:item.name,value:item.value}
+    getFieldKeys(key, value) {
+        let cur = {}
+        self.fields.forEach((item) => {
+            if (item.name === key) {
+                cur[item.name] = { name: item.name, value: value }
+            } else {
+                cur[item.name] = { name: item.name, value: item.value }
             }
-            
+
         })
         return cur
     },
