1e12198a0791642a9a4367200b5e298c723bce40
TingzhouJia <joe.tzjia@gmail.com>
2021-04-15 21:12:37 -0400

    utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import React from "react";
-import {isFragment} from 'react-is'
+import { isFragment } from 'react-is'
+import { InternalNamePath } from "./interface";
 export function warning(valid: boolean, message: string) {
   if (process.env.NODE_ENV !== 'production' && !valid && console !== undefined) {
     console.error(`Warning: ${message}`);
@@ -37,4 +38,106 @@ export function defaultGetValueFromEvent(valuePropName: string, ...args: any[]) 
   }
 
   return event;
+}
+
+
+function internalSet<Entity = any, Output = Entity, Value = any>(
+  entity: Entity,
+  paths: (string | number)[],
+  value: Value,
+  removeIfUndefined: boolean,
+): Output {
+  if (!paths.length) {
+    return (value as unknown) as Output;
+  }
+
+  const [path, ...restPath] = paths;
+
+  let clone: Output;
+  if (!entity && typeof path === 'number') {
+    clone = ([] as unknown) as Output;
+  } else if (Array.isArray(entity)) {
+    clone = ([...entity] as unknown) as Output;
+  } else {
+    clone = ({ ...entity } as unknown) as Output;
+  }
+
+  // Delete prop if `removeIfUndefined` and value is undefined
+  if (removeIfUndefined && value === undefined && restPath.length === 1) {
+    delete clone[path][restPath[0]];
+  } else {
+    clone[path] = internalSet(clone[path], restPath, value, removeIfUndefined);
+  }
+
+  return clone;
+}
+
+export function set<Entity = any, Output = Entity, Value = any>(
+  entity: Entity,
+  paths: (string | number)[],
+  value: Value,
+  removeIfUndefined: boolean = false,
+): Output {
+  // Do nothing if `removeIfUndefined` and parent object not exist
+  if (
+    paths.length &&
+    removeIfUndefined &&
+    value === undefined &&
+    !get(entity, paths.slice(0, -1))
+  ) {
+    return (entity as unknown) as Output;
+  }
+
+  return internalSet(entity, paths, value, removeIfUndefined);
+}
+
+
+export function get(entity: any, path: (string | number)[]) {
+  let current = entity;
+
+  for (let i = 0; i < path.length; i += 1) {
+    if (current === null || current === undefined) {
+      return undefined;
+    }
+
+    current = current[path[i]];
+  }
+
+  return current;
+}
+export function setValue(
+  store: any,
+  namePath: InternalNamePath,
+  value: any,
+  removeIfUndefined = false,
+): any {
+  const newStore = set(store, namePath, value, removeIfUndefined);
+  return newStore;
+}
+export function setValues<T>(store: T, ...restValues: T[]): T {
+  return restValues.reduce(
+    (current: T, newStore: T): T => internalSetValues<T>(current, newStore),
+    store,
+  );
+}
+function internalSetValues<T>(store: T, values: T): T {
+  const newStore: T = (Array.isArray(store) ? [...store] : { ...store }) as T;
+
+  if (!values) {
+    return newStore;
+  }
+
+  Object.keys(values).forEach(key => {
+    const prevValue = newStore[key];
+    const value = values[key];
+
+    // If both are object (but target is not array), we use recursion to set deep value
+    const recursive = isObject(prevValue) && isObject(value);
+    newStore[key] = recursive ? internalSetValues(prevValue, value || {}) : value;
+  });
+
+  return newStore;
+}
+function isObject(obj: any) {
+  return typeof obj === 'object' && obj !== null && Object.getPrototypeOf(obj) === Object.prototype;
 }
