6555125ecfeb927f39c0c200bb11ee0f9271b1e4
TingzhouJia <joe.tzjia@gmail.com>
2021-04-17 15:45:45 -0400

    list form

--- src/ListForm.tsx
+++ src/ListForm.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect } from "react";
 import { useMst } from "./context";
 import { Meta, NamePath, ValidatorRule } from "./interface";
@@ -11,28 +12,34 @@ export interface ListOperations {
 
 export interface ListProps {
     name: string;
-    rules?: ValidatorRule[];
+    // rules?: ValidatorRule[];
     validateTrigger?: string | string[] | false;
     initialValue?: any[];
     children?: (
         fields: any[],
         operations: ListOperations,
-        errors: string[],
+
     ) => JSX.Element | React.ReactNode;
 }
 
-const List: React.FC<ListProps> = ({
-    children,
-    rules, validateTrigger,
-    initialValue,
-    name
-}) => {
+const List: React.FC<ListProps> = (props) => {
+    const {
+        children,
+        validateTrigger,
+        initialValue,
+        name
+    } = props
     if (typeof children !== 'function') {
         warning(false, 'Form.List only accepts function as children.');
         return null;
     }
 
     const { store } = useMst()
+    const keyRef = React.useRef({
+        keys: [],
+        id: 0,
+    });
+    const keyManager = keyRef.current;
 
     const initData = () => {
         if (!Array.isArray(initialValue)) {
@@ -40,6 +47,8 @@ const List: React.FC<ListProps> = ({
         }
         if (initialValue) {
             store.registerFromForm(name, initialValue)
+        } else {
+            store.registerInit(name)
         }
     }
     useEffect(() => {
@@ -57,21 +66,46 @@ const List: React.FC<ListProps> = ({
                 warning(false, "index cannot be negative")
                 return
             } else if (index >= length || !index) {
+                keyManager.keys = [...keyManager.keys, keyManager.id];
                 store.addListValue(name, defaultValue)
             } else if (index < length) {
+                keyManager.keys = [
+                    ...keyManager.keys.slice(0, index),
+                    keyManager.id,
+                    ...keyManager.keys.slice(index),
+                ];
                 store.addListValue(name, defaultValue, index)
 
             }
+            keyManager.id += 1;
         },
         remove: (index: number) => {
             const length = store.listLength(name)
             if (index >= length) {
                 return warning(false, "Cannot delete outscoped value")
             }
+            keyManager.keys = keyManager.keys.filter((_, keysIndex) => keysIndex === index);
             store.removeListValue(name, index)
         }
     }
 
-    return children([],Operations,{})
+    return (<>{
+        children(store.getListData(name).map((item, index) => {
+            let key = keyManager.keys[index];
+            if (key === undefined) {
+                keyManager.keys[index] = keyManager.id;
+                key = keyManager.keys[index];
+                keyManager.id += 1;
+            }
+            let errors = Object.keys(item).map(each => {
+                return item.get(each).error
+            })
+            return {
+                errors, name, isListField: true, fieldKey: key
+            }
+        }), Operations)
+    }</>)
 
 }
+
+export default List
