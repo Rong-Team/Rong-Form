dc512b4adb71ff4682165750e7c59c10bc96d91c
TingzhouJia <joe.tzjia@gmail.com>
2021-04-22 14:06:32 -0400

    :rocket: validate demo

---
+++ docs/example/validate.tsx
@@ -0,0 +1,22 @@
+import {Form,Field} from '../../src'
+export default ()=>{
+    return (
+        <Form>
+            <Field name="name" rules={{required:true,}}>
+                {
+                    (control,meta)=>{
+                     console.log(meta)
+                    return <>{
+                        <input {...control}/>
+                    }
+                   
+                    {
+                        meta.errors?.length>0?meta.errors.map(item=>item):""
+                    }
+                    </>
+                    }
+                }
+            </Field>
+        </Form>
+    )
+}
